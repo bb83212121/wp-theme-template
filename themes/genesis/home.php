7fe4f069aaec40d93d029bf10a79b6462be3b721
Jonas Beck <jonasbeck@Jonass-MacBook-Pro.local>
2025-04-08 01:01:22 +0900

    home page

---
+++ themes/genesis/home.php
@@ -0,0 +1,15 @@
+<?php
+  if ( ! defined( 'ABSPATH' ) ) exit;
+  get_header();
+?>
+<script type="text/javascript">
+  $(() => {
+  })
+  $(window).on('load', function () {
+  })
+</script>
+
+<main id="main_content" class="main_content home-page">
+</main
+
+<?php get_footer(); ?>
